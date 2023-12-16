49b5871788425fe29c5bb79d94993324e2da080b
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-12-16 09:30:43 +0700

    update module people:

--- config/lang/id/label.php
+++ config/lang/id/label.php
@@ -10,4 +10,5 @@ return [
     'name'  => 'Nama',
     'email' => 'Email',
     'phone' => 'No. HP',
+    'detail' => 'Detail',
 ];
---
+++ hooks/action-button-people.php
@@ -0,0 +1,3 @@
+<?php
+
+return '<a href="'.routeTo('people/detail',['id'=>$data->id]).'" class="btn btn-sm btn-info"><i class="fas fa-eye"></i> '.__('people.label.detail').'</a> ';
